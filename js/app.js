aa35e3315b12990b453bacde23640b3ae7a938ea
tunjiakanbi <tunjiakanbi@gmail.com>
2021-11-12 15:09:36 -0600

    added starter animation to svg images

--- js/app.js
+++ js/app.js
@@ -1,32 +1,42 @@
 $(function () {
-function interSect(){
+  function interSect() {
     const nav = document.querySelector("nav");
-const intro = document.querySelector(".intro-content");
-const intro_Options = {
-    rootMargin: '-750px 0px 0px 0px',
-    // threshold: 1
-};
+    const intro = document.querySelector(".intro-content");
+    const intro_Options = {
+      rootMargin: '-750px 0px 0px 0px',
+      // threshold: 1
+    };
 
-const introObserver = new IntersectionObserver(function (
-  entries,
-  introObserver
-) {
-  entries.forEach((entry) => {
-      console.log(entry);
-    if (!entry.isIntersecting) {
-      nav.classList.add("scrolled");
-    }
-    else {
-        nav.classList.remove("scrolled");
-    }
-  });
-},
-intro_Options);
+    const introObserver = new IntersectionObserver(function (
+        entries,
+        introObserver
+      ) {
+        entries.forEach((entry) => {
+          console.log(entry);
+          if (!entry.isIntersecting) {
+            nav.classList.add("scrolled");
+          } else {
+            nav.classList.remove("scrolled");
+          }
+        });
+      },
+      intro_Options);
 
-introObserver.observe(intro);
+    introObserver.observe(intro);
+  }
+
+  interSect();
+
+
+  const svgAssests = $('.mySVG');
+console.log(svgAssests);
+function init() {
+  let tl = gsap.timeline();
+tl.to('div.intro-content', {autoAlpha: 1, duration: 3}, '+=2');
+tl.from(svgAssests, {xPercent: -100, autoAlpha: 0, stagger: 0.2, ease: 'elastic.inOut(1, 0.3)'});
 }
- 
-interSect();
+
+init();
   ////////////////////////document ready function ends here
 });
 
