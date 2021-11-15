145454e06c65e0e019c5a1451a0d1ceca8330e71
tunjiakanbi <tunjiakanbi@gmail.com>
2021-11-15 09:49:30 -0600

    minor tweaks in css and js files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(function () {
+  gsap.registerPlugin(ScrollTrigger);
   function interSect() {
     const nav = document.querySelector("nav");
     const intro = document.querySelector(".intro-content");
@@ -29,15 +30,61 @@ $(function () {
 
 
   const svgAssests = $('.mySVG');
-console.log(svgAssests);
-function init() {
-  let tl = gsap.timeline();
-tl.to('div.intro-content', {autoAlpha: 1, duration: 3}, '+=2');
-tl.from(svgAssests, {xPercent: -100, autoAlpha: 0, stagger: 0.2, ease: 'elastic.inOut(1, 0.3)'});
-}
+  //const svgGroup1 = document.querySelectorAll('svg#Group_4.mySVG > path');
+  //const svgGroup2 = document.querySelectorAll('svg#mskG_1a.mySVG > g#Mask_Group1 > #Group_1 > path');
 
-init();
+  const svgGroup1 = $('svg#Group_4.mySVG > path');
+  const svgGroup2 = $('svg#mskG_1a.mySVG > g#Mask_Group_1 > g#Group_1 > path');
+  // const svgGroup2 = $('#Group_1 > path');
+  // console.log(svgGroup1);
+  console.log(svgGroup2);
+  // console.log(svgAssests);
+  function init() {
+    let tl = gsap.timeline();
+    tl.to('div.intro-content', {
+      autoAlpha: 1,
+      duration: 3
+    }, '-=2');
+    tl.from(svgAssests, {
+      xPercent: -100,
+      autoAlpha: 0,
+      stagger: 0.2,
+      ease: 'linear'
+    },'+=2');
+    // tl.from(svgAssests, {
+    //   xPercent: -100,
+    //   autoAlpha: 0,
+    //   stagger: 0.2,
+    //   ease: 'elastic.inOut(1, 0.3)'
+    // });
+    tl.fromTo(svgGroup1, {
+      yPercent: -100,
+      autoAlpha: 0
+    }, {
+      yPercent: 0,
+      autoAlpha: 1,
+      stagger: 0.01
+    })
+    tl.fromTo(svgGroup2, {
+      autoAlpha: 0
+    }, {
+      autoAlpha: 1,
+      stagger: 0.2
+    },'-=3')
+  
+
+  }
+
+
+
+  init();
+    const scroll = new LocomotiveScroll({
+      // el: document.querySelector('body'),
+      el: document.querySelector('.intro-content'),
+      smooth: true,
+      // offset: ["10vh", "0"],
+      // initPosition: {x:0, y: "10vh"},
+      lerp: 0.1
+  });
   ////////////////////////document ready function ends here
 });
-
-
